42e4680f4b27ba551e11f4ca00cfd38bc850180c
Antonio Solo <as@solotony.com>
2021-02-26 22:33:12 +0300

    phpmailer

--- mail/mail.php
+++ mail/mail.php
@@ -1,44 +1,47 @@
 <?php
-// несколько получателей
-$to  = 'alex17b@yandex.ru' . ', '; // обратите внимание на запятую
-$to .= 'peter.butrimoff@yandex.ru';
-// тема письма
-$subject = 'Заявка с сайта';
-// текст письма
-$message = '
-<html>
-<head>
-  <title>Заявка с сайта</title>
-</head>
-<body>
-  <p>Содержание</p>
-  <table>
-    <tr>
-      <td>Имя</td>
-      <td>'.$_POST['name'].'</td>
-    </tr>
-    <tr>
-      <td>Телефон</td>
-      <td>'.$_POST['phone'].'</td>
-    </tr>
-    <tr>
-      <td>Сообщение</td>
-      <td>'.$_POST['skype'].'</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
-// Для отправки HTML-письма должен быть установлен заголовок Content-type
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-// Дополнительные заголовки
-// $headers .= 'To: Mary <info@lending.rguitar.ru>' . "\r\n";
-$headers .= 'From: ЗВУКОЗАПИСЬ РОНДО <info@lending.rguitar.ru>' . "\r\n";
-// $headers .= 'Cc: info@lending.rguitar.ru' . "\r\n";
-// $headers .= 'Bcc: info@lending.rguitar.ru' . "\r\n";
-// Отправляем
-mail($to, $subject, $message, $headers);
-header('Location: /spasibo');
-?>
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
